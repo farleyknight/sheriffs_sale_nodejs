7f99a23bae3e3bcb9014b0c484cdc318871d933e
Farley Knight <farleyknight@gmail.com>
2014-07-05 11:57:19 -0400

    Updating package.json

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next){
 });
 
 
-var haversineSQL = function(distance, latitude, longitude, lat_attr, lng_attr) {
+var haversineSQL = function(latitude, longitude, lat_attr, lng_attr) {
   var earth = 3958.755864232; // Radius of earth in miles
 
   var sql = "(" + earth + " * 2 * ASIN(SQRT(" +
@@ -42,22 +42,15 @@ var haversineSQL = function(distance, latitude, longitude, lat_attr, lng_attr) {
   return sql;
 };
 
-var distanceWhere = function(distance, latitude, longitude, lat_attr, lng_attr) {
-  var earth = 3956; // Radius of earth in miles
-
-  return haversineSQL(latitude, longitude, lat_attr, lng_attr)
+var distanceWhere = function(distance, lat, lng, lat_attr, lng_attr) {
+  return haversineSQL(lat, lng, lat_attr, lng_attr)
     + " BETWEEN 0 and " + distance;
 };
 
-var distanceAttr = function(latitude, longitude) {
-  var earth = 3958.755864232; // Radius of earth in miles
-
-  var sql = haversineSQL(latitude, longitude, 'properties.latitude', '');
-    "(" + earth + " * 2 * ASIN(SQRT(" +
-    "POWER(SIN((" + latitude +" - properties.latitude) * PI() / 180 / 2), 2) + " +
-    "COS(" + latitude + " * PI() / 180) * COS(properties.latitude * PI() / 180) * " +
-    "POWER(SIN((" + longitude + " - properties.longitude) * PI() / 180 / 2), 2)" +
-    "))) as distance";
+var distanceSelect = function(latitude, longitude) {
+  var sql =
+    haversineSQL(latitude, longitude, 'properties.latitude', 'properties.longitude')
+    + " as distance";
 
   return Sequelize.literal(sql);
 };
@@ -70,21 +63,22 @@ app.get('/properties.json', function(req, res) {
   var min_price = req.query.min_price      ||  0;
   var lat       = parseFloat(req.query.lat || 39.97);
   var lng       = parseFloat(req.query.lng || -75.15);
-  var count     = req.query.count          || 10;
+  var count     = req.query.count          || 200;
 
   var query = Property.findAll({
-    attributes: ['*', distanceAttr(lat, lng)],
+    attributes: ['*', distanceSelect(lat, lng)],
 
     where: Sequelize.and(
       ['price_in_dollars < ?', max_price * 1000],
       ['price_in_dollars > ?', min_price * 1000],
-      haversineSQL(20, lat, lng, 'latitude', 'longitude')
+      distanceWhere(20, lat, lng, 'latitude', 'longitude')
     ),
 
     order: 'distance asc',
 
     limit: count
   });
+
   query.success(function(properties) {
     res.send(properties);
   });
